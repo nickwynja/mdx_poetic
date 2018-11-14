01aaae42d64d37cca786e7a57b9b6b5aae5d8eb3
Nick Wynja <nick@nickwynja.com>
2018-11-14 11:22:33 -0500

    Define divs for poem content

--- poetic.py
+++ poetic.py
@@ -1,91 +1,40 @@
-from markdown.extensions import Extension
-from markdown.blockprocessors import BlockProcessor, ListIndentProcessor
-from markdown.util import etree
 import re
-
-class PoeticProcessor(BlockProcessor):
-    """ Process Definition Lists. """
-
-    RE = re.compile(r'(^|\n)[ ]{0,3}:[ ]{1,3}(.*?)(\n|$)')
-    NO_INDENT_RE = re.compile(r'^[ ]{0,3}[^ :]')
-
-    def test(self, parent, block):
-        return bool(self.RE.search(block))
-
-    def run(self, parent, blocks):
-
-        raw_block = blocks.pop(0)
-        m = self.RE.search(raw_block)
-        terms = [l.strip() for l in
-                 raw_block[:m.start()].split('\n') if l.strip()]
-        block = raw_block[m.end():]
-        no_indent = self.NO_INDENT_RE.match(block)
-        if no_indent:
-            d, theRest = (block, None)
-        else:
-            d, theRest = self.detab(block)
-        if d:
-            d = '%s\n%s' % (m.group(2), d)
-        else:
-            d = m.group(2)
-        sibling = self.lastChild(parent)
-        if not terms and sibling is None:
-            # This is not a definition item. Most likely a paragraph that
-            # starts with a colon at the beginning of a document or list.
-            blocks.insert(0, raw_block)
-            return False
-        if not terms and sibling.tag == 'p':
-            # The previous paragraph contains the terms
-            state = 'looselist'
-            terms = sibling.text.split('\n')
-            parent.remove(sibling)
-            # Acquire new sibling
-            sibling = self.lastChild(parent)
-        else:
-            state = 'list'
-
-        if sibling is not None and sibling.tag == 'dl':
-            # This is another item on an existing list
-            dl = sibling
-            if not terms and len(dl) and dl[-1].tag == 'dd' and len(dl[-1]):
-                state = 'looselist'
-        else:
-            # This is a new list
-            dl = etree.SubElement(parent, 'dl')
-        # Add terms
-        for term in terms:
-            dt = etree.SubElement(dl, 'dt')
-            dt.text = term
-        # Add definition
-        self.parser.state.set(state)
-        dd = etree.SubElement(dl, 'dd')
-        self.parser.parseBlocks(dd, [d])
-        self.parser.state.reset()
-
-        if theRest:
-            blocks.insert(0, theRest)
-
-
-class PoeticIndentProcessor(ListIndentProcessor):
-    """ Process indented children of definition list items. """
-
-    ITEM_TYPES = ['dd']
-    LIST_TYPES = ['dl']
-
-    def create_item(self, parent, block):
-        """ Create a new dd and parse the block with it as the parent. """
-        dd = etree.SubElement(parent, 'dd')
-        self.parser.parseBlocks(dd, [block])
-
+from markdown.preprocessors import Preprocessor
+from markdown.postprocessors import Postprocessor
+from markdown.extensions import Extension
 
 class PoeticExtension(Extension):
-    """ Add definition lists to Markdown. """
-
     def extendMarkdown(self, md, md_globals):
-        """ Add an instance of PoeticProcessor to BlockParser. """
-        md.parser.blockprocessors.register(PoeticIndentProcessor(md.parser), 'defindent', 85)
-        md.parser.blockprocessors.register(PoeticProcessor(md.parser), 'deflist', 25)
+        md.registerExtension(self)
+        md.preprocessors.add(
+            "poem_formatter", PoemFormatter(md), ">html_block")
 
+class PoemFormatter(Preprocessor):
+    def run(self, lines):
+        open_syntax = "|||:"
+        close_syntax = "|||"
+        out = []
+        inside_poem = False
+        for idx, line in enumerate(lines):
+            if line == open_syntax:
+                inside_poem = True
+                # Remove blank opening line
+                if lines[idx + 1] == '':
+                    del lines[idx + 1]
+                out.append('<div class="mdx-poem"><div class="mdx-peom--stanza">')
+            elif inside_poem == True and line != close_syntax:
+                if line == '':
+                    if lines[idx + 1] != close_syntax:
+                        out.append('</div><div class="mdx-poem--stanza">')
+                else:
+                    out.append('<div class="mdx-poem--line">%s</div>' % line.strip())
+            elif inside_poem == True and line == close_syntax:
+                inside_poem = False
+                out.append('</div></div>')
+            else:
+                # Return line untouched
+                out.append(line)
+        return out
 
-def makeExtension(**kwargs):  # pragma: no cover
-    return PoeticExtension(**kwargs)
+def makeExtension(configs={}):
+    return PoeticExtension(configs=configs)
--- testing.py
+++ testing.py
@@ -1,4 +1,21 @@
 import markdown
 from poetic import PoeticExtension
 
-print(markdown.markdown('Apple\n:   Pomaceous fruit of plants of the genus Malus in\n    the family Rosaceae.', extensions=[PoeticExtension()]))
+md = """
+This is just some normal text that is prose. It might have some _markdown_.
+
+|||:
+
+    this is a
+    line of a poem
+
+    and this is _another_
+    stanza of a poem
+    with **markdown** in it
+
+|||
+
+And then this is some **more** markdown that follows.
+"""
+
+print(markdown.markdown(md, extensions=['fenced_code', PoeticExtension()]))
