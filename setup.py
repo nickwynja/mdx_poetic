0236be2dba59cedb14ebcc74d446b7a5e462c870
Nick Wynja <nick@nickwynja.com>
2018-11-19 10:55:56 -0500

    rename module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
-    name='Poetic',
-    version='1.0',
-    py_modules=['poetic'],
+    name='mdx_poetic',
+    version='0.1',
+    py_modules=['mdx_poetic'],
     install_requires = ['markdown>=2.5'],
 )
