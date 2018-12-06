319c33fb5c8d9bafead0828136da762c0f62866d
Nick Wynja <nick@nickwynja.com>
2018-12-05 21:36:01 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='mdx_poetic',
     author='Nick Wynja',
     description='Formats poetry in Markdown',
-    long_description=readme(),
-    long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/nickwynja/mdx_poetic',
-    version='0.6',
+    version='0.8',
     py_modules=['mdx_poetic'],
     install_requires = ['markdown>=2.5'],
 )
